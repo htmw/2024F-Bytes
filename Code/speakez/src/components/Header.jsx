a1b57af49fa6bc1e1eb3d68b0af873a1060fd6a7
Venkat Sai Pallapu <venkatsai.pallapu@finmkt.io>
2024-10-15 18:33:43 -0400

    logo path change

--- Code/speakez/src/components/Header.jsx
+++ Code/speakez/src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from '../../public/images/SpeakEZLogo.jpg'
 
 const Header = () => {
   return (
     <header className="logo-image">
-      <img src={logo} alt="Logo" />
+      <img src="/images/SpeakEZLogo.jpg" alt="Logo" />
     </header>
   );
 };
