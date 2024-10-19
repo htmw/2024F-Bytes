1e9067717b71b962bd2a922887493d59616290b4
Ttan-24 <tanviprakash24@gmail.com>
2024-10-19 14:22:09 -0400

    Error "failed to load module script" fixed + transcribed text shows successfully in the console

--- Code/speakez/src/App.jsx
+++ Code/speakez/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
 import SignInDropdown from "./components/SignInDropdown";
 // import LanguageSelection from "./components/LanguageSelection";
@@ -18,83 +18,103 @@ import { MessageTypes } from "./utils/reference";
 
 const App = () => {
   //const [loading, setLoading] = useState(false);
-  
-  // const [downloading, setDownloading] = useState(false);
-  // const [output, setOutput] = useState(null);
-  // const [finished, setFinished] = useState(false);
 
-  // /*create a worker reference, to get the ml code running in the background*/
-  // const worker = useRef(null);
+  const [downloading, setDownloading] = useState(false);
+  const [result, setResult] = useState(null);
+  const [done, setDone] = useState(false);
 
-  // useEffect(() => {
-  //   /*if currently there is no worker then create a new one from the whisper.worker.js */
-  //   if (!worker.current) {
-  //     worker.current = new Worker(
-  //       new URL("./utils/whisper.worker.js", import.meta.url),
-  //       {
-  //         type: "module",
-  //       }
-  //     );
-  //   }
-  //   /*switch cases, worker can communicate with the status of the transcription, handles incoming messages from the worker */
-  //   const onMessageReceived = async (e) => {
-  //     switch (e.data.type) {
-  //       case "DOWNLOADING":
-  //         setDownloading(true);
-  //         console.log("DOWNLOADING");
-  //         break;
-  //       // case "LOADING":
-  //       //   setLoading(true);
-  //       //   console.log("LOADING");
-  //       //   break;
-  //       case "RESULT":
-  //         setOutput(e.data.results);
-  //         console.log(e.data.results);
-  //         break;
-  //       case "INFERENCE_DONE":
-  //         setFinished(true);
-  //         console.log("DONE");
-  //         break;
-  //     }
-  //   };
+  /*create a worker reference, to get the ml code running in the background*/
+  const worker = useRef(null);
 
-  //   worker.current.addEventListener("message", onMessageReceived);
+  useEffect(() => {
+    /*if currently there is no worker then create a new one from the whisper.worker.js */
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("./utils/whisper.worker.js", import.meta.url),
+        {
+          type: "module",
+        }
+      );
+    }
+    /*switch cases, worker can communicate with the status of the transcription, handles incoming messages from the worker */
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case "DOWNLOADING":
+          setDownloading(true);
+          console.log("DOWNLOADING");
+          break;
+        // case "LOADING":
+        //   setLoading(true);
+        //   console.log("LOADING");
+        //   break;
+        case "RESULT":
+          setResult(e.data.results);
+          console.log(e.data.results);
+          break;
+        case "INFERENCE_DONE":
+          setDone(true);
+          console.log("DONE");
+          break;
+      }
+    };
 
-  //   // define a cleanup function, to prevent memory leaks
-  //   return () =>
-  //     worker.current.removeEventListener("message", onMessageReceived);
-  // });
+    worker.current.addEventListener("message", onMessageReceived);
 
+    // define a cleanup function, to prevent memory leaks
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
+  });
+
+  const [file, setFile] = useState(null);
+
+  function deleteSelectedFile() {
+    setFile(null);
+  }
+  async function readFromUploadFile(file) {
+    const sampling_rate = 16000;
+    const audioContext = new AudioContext({ sampleRate: sampling_rate });
+    // reads the audio file and converts it into an array buffer
+    const readAudioFile = await file.arrayBuffer();
+    // decodes buffer into audio data
+    const decodedAudioData = await audioContext.decodeAudioData(readAudioFile);
+    // gets audio data from the first channel
+    const finalAudioData = decodedAudioData.getChannelData(0);
+    // returns processed audio data, transforming the file into a format to manipulate
+    return finalAudioData;
+  }
+
+  //const navigate = useNavigate();
+  const clickTranscribe = async () => {
+    // checks for mp3 file or audio file from recording
+    if (!file) {
+      return;
+    }
+    // readAudioFrom -> gets the data from the file
+    let audio = await readFromUploadFile(file);
+    // modal used for transcribe, sends audio data to a web worker for transcription using the whisper-model to do this
+    const model_name = `openai/whisper-tiny.en`;
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name,
+    });
+  };
 
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <SignInDropdown />
-        {/*<LanguageSelection
-        sourceLanguage={sourceLanguage}
-        targetLanguage={targetLanguage}
-        onSourceChange={(e) => setSourceLanguage(e.target.value)}
-        onTargetChange={(e) => setTargetLanguage(e.target.value)}
-        onSwap={swapLanguages}
-        languages={languages}
-      />
-      <TextBoxes />*/}
-        {/*<TranslationOptions
-        languages={languages}
-        sourceLanguage={sourceLanguage}
-        targetLanguage={targetLanguage}
-      />*/}
-
-        {/* Routes for different components */}
-        <Routes>
-          <Route path="/" element={<FileDisplay /> } />
-          <Route path="/fileDisplayTrans" element={<FileDisplayTrans />} />
-        </Routes>
-
-        {/*<FileDisplay/>*/}
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <SignInDropdown />
+      {result ? (
+        <FileDisplayTrans />
+      ) : (
+        <FileDisplay
+          file={file}
+          setFile={setFile}
+          clickTranscribe={clickTranscribe}
+          deleteSelectedFile={deleteSelectedFile}
+        />
+      )}
+    </div>
   );
 };
 
