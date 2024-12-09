947e928e9f72c5a8b3c8bb4596e74f0fd2af3c0e
Venkat Sai Pallapu <venkatsai.pallapu@finmkt.io>
2024-12-09 17:21:19 -0500

    colour changes in header and body

--- Code/speakez/src/App.jsx
+++ Code/speakez/src/App.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect, useRef } from "react";
-// NEW -Importing React Router tools to set up page navigation and handle routes in the app.
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import LoginComponent from "./components/LoginComponent";
-import "./index.css";
-
 import SignInPage from "./components/SignInPage";
 import HomeComponent from "./components/HomeComponent";
+import { ThemeProvider } from "./components/ThemeContext"; // Import ThemeProvider
+
+import "./index.css";
 
 function App() {
   return (
     <div className="h-screen flex flex-col">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomeComponent />} />
-          <Route path="/login" element={<LoginComponent />} />
-          <Route path="/signin" element={<SignInPage />} />
-        </Routes>
-      </Router>
+      <ThemeProvider> {/* Wrap your app with ThemeProvider */}
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomeComponent />} />
+            <Route path="/login" element={<LoginComponent />} />
+            <Route path="/signin" element={<SignInPage />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
